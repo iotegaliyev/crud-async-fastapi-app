e0a1194deb3252a12d0614f1c240132a94b5e4b6
Islam Otegaliyev <iotegaliyev@gmail.com>
2024-08-31 18:45:52 +0500

    Create Read for products

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from src.models import Base, db_helper
+from src.config import settings
+from api_v1 import router as router_v1
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+
+
+@app.get('/')
+async def hello():
+    return {"message": "Hello World!"}
